6e46e88a874decb0f89ca0fe44567e858f2ff1ad
Knyyl <101050874+Knyyl@users.noreply.github.com>
2025-04-18 21:02:56 +0200

    Added Logo

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,14 +1,24 @@
 package main;
 
 import javax.swing.*;
-
+import java.awt.*;
+import javax.imageio.ImageIO;
+import java.io.File;
+import java.io.IOException;
 
 public class Main {
     public static void main(String[] args) {
         JFrame window = new JFrame();
         window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         window.setResizable(false);
-        window.setTitle("Spielnameplatzhalter");
+        window.setTitle("NAOI");
+
+        try {
+            Image icon = ImageIO.read(new File("res/icon/ACABI.png"));
+            window.setIconImage(icon);
+        } catch (IOException e) {
+            System.out.println("Could not load icon. ");
+        }
 
         GamePanel gamePanel = new GamePanel();
         window.add(gamePanel);
@@ -19,8 +29,5 @@ public class Main {
         window.setVisible(true);
 
         //gamePanel.startGameThread();
-
-
     }
 }
-
