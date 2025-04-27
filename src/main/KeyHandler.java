9df896b8b2e1457e20092a3626dbb944f7070122
Knyyl <101050874+Knyyl@users.noreply.github.com>
2025-04-27 21:36:16 +0200

    Update KeyHandler.java

--- src/main/KeyHandler.java
+++ src/main/KeyHandler.java
@@ -6,7 +6,7 @@ import java.awt.event.KeyListener;
 public class KeyHandler implements KeyListener {
 
     public boolean upPressed, downPressed, leftPressed, rightPressed;
-    public boolean enterPressed;  // Add this to handle Enter key
+    public boolean enterPressed;
 
     @Override
     public void keyTyped(KeyEvent e) {
